d08e9fe96cff878b6649f514c3704881234ed20e
Marcus Sena <marcuseduardo.ns@gmail.com>
2024-05-01 10:59:56 -0400

    4 different models for colors

--- cor.py
+++ cor.py
@@ -13,7 +13,6 @@ desired_height = 400
 resized_image = cv.resize(original_img, (desired_width, desired_height))
 
 
-
 ## Define the custom filter (R = 191,  G = 154, B = 208)
 custom_filter_beta = np.zeros_like(resized_image)
 custom_filter_beta[:, :, 0] = 191             # Set the blue channel to 191 
@@ -26,11 +25,25 @@ custom_filter_image_beta = cv.addWeighted(resized_image, 0.7, custom_filter_beta
 ## Define the custom filter (R = 42, G = -82, B = 98)
 custom_filter_alfa = np.zeros_like(resized_image)
 custom_filter_alfa[:, :, 0] = np.clip(42, 0, 255)  # Clip to ensure the value is within the valid range
-custom_filter_alfa[:, :, 1] = np.clip(255 - 82, 0, 255)  # Adjusted green channel
+custom_filter_alfa[:, :, 1] = np.clip(-82, 0, 255)  # Adjusted green channel
 custom_filter_alfa[:, :, 2] = np.clip(98, 0, 255)  # Clip to ensure the value is within the valid range
 
 ## Apply the custom RGB filter to the image
 custom_filter_image_alfa = cv.addWeighted(resized_image, 0.7, custom_filter_alfa, 0.3, 0)
+
+custom_filter_image_omega = resized_image.copy()
+
+red_channel = custom_filter_image_omega[:, :, 2]
+red_channel_modified = np.clip(red_channel + 42, 0, 255)
+custom_filter_image_omega[:, :, 2] = red_channel_modified
+
+green_channel = custom_filter_image_omega[:, :, 1]
+green_channel_modified = np.clip(green_channel - 82, 0, 255)
+custom_filter_image_omega[:, :, 1] = green_channel_modified
+
+blue_channel = custom_filter_image_omega[:, :, 0]
+blue_channel_modified = np.clip(blue_channel + 98, 0, 255)
+custom_filter_image_omega[:, :, 0] = blue_channel_modified
 
 
 ## Alternative without the green color
@@ -39,17 +52,21 @@ purple_filter_image[:, :, 0] = resized_image[:, :, 0]  # Keep the blue channel a
 purple_filter_image[:, :, 1] = 0               # Set the green channel to 0 (remove green)
 purple_filter_image[:, :, 2] = resized_image[:, :, 2]  # Keep the red channel as it is
 
+
+
 ## Save Images
 # Save the original and filtered images
 cv.imwrite('./output/original_image.jpg', resized_image)
-cv.imwrite('./output/custom_filtered_image.jpg', custom_filter_image_beta)
-cv.imwrite('./output/custom_filtered_image.jpg', custom_filter_image_alfa)
+cv.imwrite('./output/custom_filtered_image_beta.jpg', custom_filter_image_beta)
+cv.imwrite('./output/custom_filtered_image_alfa.jpg', custom_filter_image_alfa)
+cv.imwrite('./output/custom_filtered_image_omega.jpg', custom_filter_image_omega)
 cv.imwrite('./output/purple_filtered_image.jpg', purple_filter_image)
 
 ## Show the images
 cv.imshow('Original Image', resized_image)
 cv.imshow('Filtered Image Alfa', custom_filter_image_alfa)
 cv.imshow('Filtered Image Beta', custom_filter_image_beta)
+cv.imshow('Filtered Image Omega', custom_filter_image_omega)
 cv.imshow('Purple Image', purple_filter_image)
 cv.waitKey(0)
 cv.destroyAllWindows()
